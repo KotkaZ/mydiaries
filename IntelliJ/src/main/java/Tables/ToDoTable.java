b6107fc6b688c17eab15a0295745de50850fb620
Tiit Vaino <tiitvaino25@gmail.com>
2020-04-18 23:29:56 +0300

    Made functionality to show today's tasks

--- IntelliJ/src/main/java/Tables/ToDoTable.java
+++ IntelliJ/src/main/java/Tables/ToDoTable.java
@@ -1,6 +1,11 @@
 package Tables;
 
+import Entries.ToDoTableEntry;
 import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ToDoTable extends DefaultTable<Entries.ToDoTableEntry> {
 
@@ -8,6 +13,34 @@ public class ToDoTable extends DefaultTable<Entries.ToDoTableEntry> {
     public void addData(String type, LocalDate deadline, String description, int priority) {
         Entries.ToDoTableEntry entries = new Entries.ToDoTableEntry(type, deadline, description, priority);
         super.addData(entries);
+    }
+
+    /**
+     * Returns sorted list of tasks, that deadlines is on given date.
+     *
+     * @param toDoTable
+     * @param date
+     * @return
+     */
+    public static List<ToDoTableEntry> getTasks4Date(ToDoTable toDoTable, LocalDate date){
+        List<ToDoTableEntry> list = new ArrayList<>();
+        for (ToDoTableEntry entry :
+                toDoTable.getTabel()) {
+            if (entry.getDeadline() == date)
+                list.add(entry);
+        }
+        Collections.sort(list);
+        return list;
+    }
+
+    /**
+     * Returns sorted list of today's tasks.
+     *
+     * @param toDoTable
+     * @return
+     */
+    public static List<ToDoTableEntry> getTodaysTasks(ToDoTable toDoTable){
+        return getTasks4Date(toDoTable, LocalDate.now());
     }
 
 
@@ -30,5 +63,6 @@ public class ToDoTable extends DefaultTable<Entries.ToDoTableEntry> {
         return orderedByExpDate;
     }*/
 }
+
 
 
