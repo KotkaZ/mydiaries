cc4a47e3799f2a8a18a48549967491d7032bc401
Tiit Vaino <tiitvaino25@gmail.com>
2020-04-18 23:43:31 +0300

    Made functionality to show today's expiring food

--- IntelliJ/src/main/java/Tables/FoodTable.java
+++ IntelliJ/src/main/java/Tables/FoodTable.java
@@ -1,6 +1,11 @@
 package Tables;
 
+import Entries.FoodTableEntry;
 import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FoodTable extends DefaultTable<Entries.FoodTableEntry> {
 
@@ -8,6 +13,34 @@ public class FoodTable extends DefaultTable<Entries.FoodTableEntry> {
     public void addData(String type, LocalDate expDate, int amount) {
         Entries.FoodTableEntry entries = new Entries.FoodTableEntry(type, expDate,  amount);
         super.addData(entries);
+    }
+
+    /**
+     * Returns sorted list of FoodTableEntry object list
+     *
+     * @param foodTable
+     * @param date
+     * @return
+     */
+    public static List<FoodTableEntry> getExp4Date(FoodTable foodTable, LocalDate date){
+        List<FoodTableEntry> list = new ArrayList<>();
+        for (FoodTableEntry entry :
+                foodTable.getTabel()) {
+            if (entry.getExpDate() == date)
+                list.add(entry);
+        }
+        Collections.sort(list);
+        return list;
+    }
+
+    /**
+     * Returns sorted list of today's expiring food.
+     *
+     * @param foodTable
+     * @return
+     */
+    public static List<FoodTableEntry> getTodaysExp(FoodTable foodTable){
+        return getExp4Date(foodTable, LocalDate.now());
     }
 
     /**
