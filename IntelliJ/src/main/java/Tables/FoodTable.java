f6253b06767071cc004354503493f71781f59b4f
Tiit Vaino <tiitvaino25@gmail.com>
2020-05-08 23:29:35 +0300

    FoodTabel getOrderedTabel proovivariant

--- IntelliJ/src/main/java/Tables/FoodTable.java
+++ IntelliJ/src/main/java/Tables/FoodTable.java
@@ -5,6 +5,7 @@ import Entries.FoodTableEntry;
 import org.joda.time.LocalDate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -62,6 +63,56 @@ public class FoodTable extends DefaultTable<Entries.FoodTableEntry> {
                 case 2: value = o1.getExpDate().compareTo(o2.getExpDate());break;
                 default:
                     throw new IllegalArgumentException("FoodTable#getOrderedTabel wrong ordering type");
+            }
+            return value  * (ascending ? 1 : -1);
+        });
+    }
+
+
+    /**
+     * This method orderes list by Entry-class object variables.
+     *
+     * @param type Int typenumber
+     *             0- type
+     *             1- inputdate
+     *             2- expDate
+     *             3- type & inputType
+     * @param ascending
+     * @return
+     */
+    public List<FoodTableEntry> getOrderedTableTry(int type, boolean ascending){
+        List<Integer> arguments;
+        switch (type){
+            case 0:
+                arguments = Arrays.asList(0);break;
+            case 1:
+                arguments = Arrays.asList(1);break;
+            case 2:
+                arguments = Arrays.asList(2);break;
+            case 3:
+                arguments = Arrays.asList(0,1);break;
+            default:
+                throw new IllegalArgumentException("FoodTable#getOrderedTabel wrong ordering type");
+        }
+        return super.getOrderedTable((o1, o2) -> {
+            int value = 0;
+            for (int elType :
+                    arguments) {
+                switch (elType) {
+
+                    case 0:
+                        value = o1.getType().compareTo(o2.getType());
+                        break;
+                    case 1:
+                        value = o1.getInputDate().compareTo(o2.getInputDate());
+                        break;
+                    case 2:
+                        value = o1.getExpDate().compareTo(o2.getExpDate());
+                        break;
+                    default:
+                        throw new RuntimeException("FoodTabel#getorderedTabel elType ");
+                }
+                if (value != 0) break;
             }
             return value  * (ascending ? 1 : -1);
         });
